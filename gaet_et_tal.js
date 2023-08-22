b4c6b90bf41110a310d87f41174ae68a893ae305
Julie <julieeriksen09@hotmail.com>
2023-08-22 10:25:47 +0200

    first commit

---
+++ gaet_et_tal.js
@@ -0,0 +1,20 @@
+// Random-funktionsgenerere
+
+const rand = Math.random() * 100;
+const tal = Math.round(rand);
+
+console.log(tal);
+
+// document.getElementById("tallet").value = tal;
+// document.write(tal);
+
+document.getElementById("knap").addEventListener("click", run);
+
+function run() {
+  const userInput = document.getElementById("userinput").value;
+  if (userInput === tal) {
+    document.getElementById("resultat").textContent = "duuuuuu vandt";
+  } else if (userInput !== tal) {
+    document.getElementById("resultat").textContent = "nooooooo";
+  }
+}
