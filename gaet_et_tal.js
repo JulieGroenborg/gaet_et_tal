d31eeac54366a88b606c680d57ef6439a7a68c57
Julie <julieeriksen09@hotmail.com>
2023-08-22 10:42:52 +0200

    Tilføjet korrekt feedback-besked

--- gaet_et_tal.js
+++ gaet_et_tal.js
@@ -13,8 +13,17 @@ document.getElementById("knap").addEventListener("click", run);
 function run() {
   const userInput = document.getElementById("userinput").value;
   if (userInput == tal) {
-    document.getElementById("resultat").textContent = "duuuuuu vandt";
-  } else if (userInput != tal) {
-    document.getElementById("resultat").textContent = "nooooooo";
+    document.getElementById("resultat").textContent =
+      "Waaauw, du gættede rigtigt. Tillykke!";
+  } else if (userInput > tal) {
+  /* else if (userInput != tal) {
+    document.getElementById("resultat").textContent =
+      "Noo.. Du gættede ikke korrekt";
+  }*/
+    document.getElementById("resultat").textContent =
+      "Du gættede desværre et for højt tal";
+  } else if (userInput < tal) {
+    document.getElementById("resultat").textContent =
+      "Du gættede desværre et for lavt tal";
   }
 }
